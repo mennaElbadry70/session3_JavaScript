90507e5ec75d080622b97b68ecea415628918095
Menna Elbadry <mennatallah701@gmail.com>
2026-01-01 20:15:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+//========= Const Var Let =================//
+// const birthYear = 2001;
+// console.log(birthYear);
+
+// let age = 24;
+// console.log(age)
+// age = 17;
+// console.log(age);
+
+//========= Math Operators =================//
+// let firstNumber = 10;
+// let secondNumber = 5;
+
+// console.log(firstNumber - secondNumber);
+// console.log(firstNumber + secondNumber);
+// console.log(firstNumber / secondNumber);
+// console.log(firstNumber * secondNumber);
+// console.log(firstNumber ** secondNumber);
+// console.log(firstNumber * 2);
+// console.log(secondNumber ** 2);
+// console.log(2 ** 5);
+// console.log(firstNumber % secondNumber);
+// console.log(--firstNumber);
+
+// ===================== Assignment operators ===============//
+// let x = 'Ali'; //assignment operator (override)
+// console.log(x);
+
+// x = 10;
+// console.log(x);
+
+// x = true;
+// console.log(x)
+
+// let y = 5;
+
+// y += 10; // y = y + 10
+// console.log(y);
+
+// y -= 3; // y = y - 3
+// console.log(y);
+
+// y *= 2; // y = y * 2
+// console.log(y);
+
+// y /= 2; // y = y / 2
+// console.log(y);
+
+// y **= 2; //y = y **2
+// console.log(y);
+
+// ===================== Comparision operators ===============//
+// gives us boolean values (true or false);
+// let nowYear = 2026
+
+// let ageAli = nowYear - 2008;
+// let ageMenna = nowYear - 2001;
+
+
+// console.log(ageAli);
+// console.log(ageMenna);
+
+// console.log(ageAli > ageMenna);
+// console.log(ageAli >= ageMenna);
+
+// console.log(ageAli < ageMenna);
+// console.log(ageAli <= ageMenna);
+
+// console.log('== loose equality checks only value');
+// let number1 = '10';
+// let number2 = 10;
+
+// console.log(number1 == number2);
+
+// console.log("=== strict equality checks value and type");
+// console.log(number1 === number2);
+
+// ===================== Logical operators ===============//
+// let ageYoussef = 70;
+// let ageTariq = 30;
+// let ageMohamed = 50;
+
+// AND operator ==> &&
+// console.log(ageMohamed > ageTariq);
+// console.log(ageMohamed > ageYoussef);
+// console.log(ageMohamed > ageTariq && ageMohamed > ageYoussef); //AND
+// console.log(ageMohamed > ageTariq || ageMohamed > ageYoussef); //OR
+
+
+
+
+
+
+
+
